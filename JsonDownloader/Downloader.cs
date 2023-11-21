32d8fab3efbcde351ec959e9cf19a4251bb5cea8
Tomáš Vytiska <tomvytiska@gmail.com>
2023-11-21 13:20:29 +0100

    Přidejte soubory projektu.

---
+++ JsonDownloader/Downloader.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+/// <summary>
+/// The <see cref="HttpClientHandler"/> class provides a simple interface for making asynchronous HTTP requests using <see cref="HttpClient"/>.
+/// </summary>
+/// <remarks>
+/// This class simplifies the management of the <see cref="HttpClient"/> instance and provides a method for asynchronously retrieving a response from a given URL.
+/// </remarks>
+public class HttpClientHandler
+{
+    /// <summary>
+    /// Gets or sets the underlying <see cref="HttpClient"/> instance used for making HTTP requests.
+    /// </summary>
+    private readonly HttpClient httpClient;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="HttpClientHandler"/> class.
+    /// </summary>
+    /// <remarks>
+    /// The constructor creates a new instance of <see cref="HttpClient"/> for handling HTTP requests.
+    /// </remarks>
+    public HttpClientHandler()
+    {
+        httpClient = new HttpClient();
+    }
+
+    /// <summary>
+    /// Performs an asynchronous HTTP GET request to the specified URL.
+    /// </summary>
+    /// <param name="url">The URL to send the HTTP GET request to.</param>
+    /// <returns>A task representing the asynchronous operation that yields an <see cref="HttpResponseMessage"/>.</returns>
+    /// <remarks>
+    /// This method initiates an asynchronous HTTP GET request using the <see cref="HttpClient"/> instance and returns the
+    /// corresponding <see cref="HttpResponseMessage"/>.
+    /// </remarks>
+    public async Task<HttpResponseMessage> GetAsync(string url)
+    {
+        return await httpClient.GetAsync(url);
+    }
+}
+
+/// <summary>
+/// The <see cref="JsonDownloader"/> class is used for downloading and processing JSON data using HTTP requests.
+/// </summary>
+/// <remarks>
+/// The class takes an instance of <see cref="HttpClientHandler"/> for handling HTTP requests. The <see cref="GetJsonAsync{JSON}"/> method
+/// retrieves JSON data from the specified URL and returns a tuple containing deserialized data, a success indicator, and any potential error.
+/// </remarks>
+public class JsonDownloader
+{
+    /// <summary>
+    /// Gets or sets the <see cref="HttpClientHandler"/> instance responsible for handling HTTP requests.
+    /// </summary>
+    private readonly HttpClientHandler httpClientHandler;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="JsonDownloader"/> class.
+    /// </summary>
+    /// <param name="httpClientHandler">The <see cref="HttpClientHandler"/> instance to be used for handling HTTP requests.</param>
+    /// <remarks>
+    /// This constructor receives an instance of <see cref="HttpClientHandler"/> to be used for making HTTP requests.
+    /// </remarks>
+    public JsonDownloader(HttpClientHandler httpClientHandler)
+    {
+        this.httpClientHandler = httpClientHandler;
+    }
+
+    /// <summary>
+    /// Asynchronously retrieves JSON data from the specified URL using the configured <see cref="HttpClientHandler"/>.
+    /// </summary>
+    /// <typeparam name="JSON">The type to which the JSON data will be deserialized.</typeparam>
+    /// <param name="url">The URL to send the HTTP GET request to.</param>
+    /// <returns>A task representing the asynchronous operation that yields a tuple containing deserialized data, a success indicator, and any potential error.</returns>
+    /// <remarks>
+    /// This method initiates an asynchronous HTTP GET request using the configured <see cref="HttpClientHandler"/> and returns a tuple containing
+    /// deserialized data, a success indicator, and any potential error encountered during the process.
+    /// </remarks>
+    public async Task<(JSON? DataJson, bool IsOk, string Error)> GetJsonAsync<JSON>(string url) where JSON : class, new()
+    {
+        JSON? jsonResponse = new();
+
+        try
+        {
+            var response = await httpClientHandler.GetAsync(url);
+
+            if (response.IsSuccessStatusCode)
+            {
+                jsonResponse = await response.Content.ReadFromJsonAsync<JSON>();
+                return (jsonResponse, true, string.Empty);
+            }
+            else
+            {
+                return (jsonResponse, false, $"{response.StatusCode} {response.ReasonPhrase} {response.IsSuccessStatusCode}");
+            }
+        }
+        catch (HttpRequestException ex)
+        {
+            return (jsonResponse, false, ex.Message);
+        }
+    }
+}
